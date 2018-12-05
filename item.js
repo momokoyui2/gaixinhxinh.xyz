d30490c2914df83d5375ff317dcf5a00ce501afd
momokoyui2 <34786849+momokoyui2@users.noreply.github.com>
2018-12-05 18:15:35 +0700

    Add files via upload

---
+++ item.js
@@ -0,0 +1,8 @@
+/**
+ * Minified by jsDelivr using UglifyJS v3.4.4.
+ * Original file: /gh/hhcdn/blogger@master/seo/item.js
+ * 
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+$(window).on("load",function(){$.when($.getScript("//cdn.jsdelivr.net/npm/lightgallery@1/dist/js/lightgallery.min.js"),$.getScript("//cdn.jsdelivr.net/npm/lg-zoom@1/dist/lg-zoom.min.js"),$.getScript("//cdn.jsdelivr.net/npm/lg-thumbnail@1/dist/lg-thumbnail.min.js"),$.Deferred(function(n){$(n.resolve)})).done(function(){$("#lightgallery").lightGallery({selector:"a[imageanchor]"})})});
+//# sourceMappingURL=/sm/9492e6d656aa6dc127c3048134581443c3ec89c13c72062de6cd7b41d30fa933.map
