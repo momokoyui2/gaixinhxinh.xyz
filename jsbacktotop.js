4483db18d27247ae5348f1a34d80f25fcbe5cc04
momokoyui2 <34786849+momokoyui2@users.noreply.github.com>
2019-01-13 11:54:53 +0000

    Update jsbacktotop.js

--- jsbacktotop.js
+++ jsbacktotop.js
@@ -1,2 +1,16 @@
-jQuery(document).ready(function(){var o=220,r=600;jQuery(window).scroll(function(){jQuery(this).scrollTop() > o?jQuery("#scrollToTop").fadeIn(r):jQuery("#scrollToTop").fadeOut(r)})});
-$(function(){$("a[href*=#]:not([href=#])").click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var t=$(this.hash);if(t=t.length?t:$("[name="+this.hash.slice(1)+"]"),t.length)return $("html,body").animate({scrollTop:t.offset().topp3},600),!1}})});
+jQuery(document).ready(function() {
+    var e = $(this);
+    jQuery(window).scroll(function() {
+        jQuery(this).scrollTop() >= 100 ? jQuery("#scrollToTop").fadeIn(250) : jQuery("#scrollToTop").fadeOut(250)
+    })
+});
+$(function() {
+    $("a[href*=#]:not([href=#])").click(function() {
+        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+            var t = $(this.hash);
+            if (t = t.length ? t : $("[name=" + this.hash.slice(1) + "]"), t.length) return $("html,body").animate({
+                scrollTop: t.offset().topp3
+            }, 500), !1
+        }
+    })
+});
