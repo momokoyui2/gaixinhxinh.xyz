1008984d90dcf1f96f2321d8e70db70a75ca2104
momokoyui2 <34786849+momokoyui2@users.noreply.github.com>
2019-01-18 10:37:35 +0000

    Update item.js

--- item.js
+++ item.js
@@ -4,5 +4,5 @@
  * 
  * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
  */
-$(window).on("load",function(){$.when($.getScript("//cdn.jsdelivr.net/npm/lightgallery@1/dist/js/lightgallery.min.js"),$.getScript("//cdn.jsdelivr.net/npm/lg-zoom@1/dist/lg-zoom.min.js"),$.getScript("//cdn.jsdelivr.net/npm/lg-thumbnail@1/dist/lg-thumbnail.min.js"),$.Deferred(function(n){$(n.resolve)})).done(function(){$("#lightgallery").lightGallery({selector:"a[imageanchor]"})})});
+$(window).on("load",function(){$.when($.getScript("//cdn.jsdelivr.net/gh/momokoyui2/groupgaixinh@b0dac85/lightgallery.min.css"),$.getScript("//cdn.jsdelivr.net/npm/lg-zoom@1/dist/lg-zoom.min.js"),$.getScript("//cdn.jsdelivr.net/npm/lg-thumbnail@1/dist/lg-thumbnail.min.js"),$.Deferred(function(n){$(n.resolve)})).done(function(){$("#lightgallery").lightGallery({selector:"a[imageanchor]"})})});
 //# sourceMappingURL=/sm/9492e6d656aa6dc127c3048134581443c3ec89c13c72062de6cd7b41d30fa933.map
